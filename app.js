467166576da43e3733856a8f1c8529813e305283
orodev <oro91dev@gmail.com>
2018-04-09 18:19:18 +0200

    second commit
    Worked on controllers and cleaning the code

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
 
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return (add(b));
-        }
-    }
+    // Some code
+    
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
-
-    //Kode
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+        
+    };
+    
+    return {
+        getinput: function() {
+            return {
+                            
+              type: document.querySelector(DOMstrings.inputType).value, 
+              description: document.querySelector(DOMstrings.inputDescription).value,
+              value: document.querySelector(DOMstrings.inputValue).value
+            
+            };
+    
+        },
+        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-
-    var z =budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
+    
+    var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+        
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event) {
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+            
         }
-    }
+        
+    });    
+        
+ };
+
+    
+    var ctrlAddItem = function() {
+        var input = UICtrl.getinput();
+  
+    };
+    
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
 
 })(budgetController, UIController);
+
+controller.init();
