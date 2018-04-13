bdc69a53220227a1fe91c2645de10844b7ae95d5
orodev <oro91dev@gmail.com>
2018-04-13 17:31:38 +0200

    added logic to the function

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
     }
     
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
     
+    var calculateTotal = function(type) {
+      var sum = 0;
+      data.allItems[type].forEach(function(cur) {
+          sum += cur.value;
+      });
+      data.totals[type] = sum;    
+    };
     
     var data = {
         allItems: {
@@ -22,7 +30,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
     
     return {
@@ -51,11 +61,41 @@ var budgetController = (function() {
             return newItem;
             
         },
+        
+        calculateBudget: function() {
+          
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            // calculate the percentage of income that we spent
+            if (data.totals.inc > 0) {
+              data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);  
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+        
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }    
+        },
+        
         testing: function() {
             console.log(data);
         }
+
     };
 })();
+
 
 // UI CONTROLLER
 var UIController = (function() {
@@ -67,6 +107,11 @@ var UIController = (function() {
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
         expenseContainer: '.expense__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
+        
         
     };
     
@@ -114,6 +159,19 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
         
+        displayBudget: function(obj) {
+            
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+            
+            if (obj.percentage > 0 ){
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                  document.querySelector(DOMstrings.percentageLabel).textContent = '---';              
+            }
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -143,6 +201,12 @@ var controller = (function(budgetCtrl, UICtrl){
 
     var updateBudget = function() {
         
+        budgetCtrl.calculateBudget();
+        
+        var budget = budgetCtrl.getBudget();
+        
+        UICtrl.displayBudget(budget);
+        
         
     }
     
@@ -171,6 +235,10 @@ var controller = (function(budgetCtrl, UICtrl){
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1});
             setupEventListeners();
         }
     };
